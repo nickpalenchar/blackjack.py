ca7bdda3e6a05bc60caaa2f2aa808f0c34219981
Nick Palenchar <nickpalenchar@users.noreply.github.com>
2017-06-19 12:00:48 -0400

    Create class.py

---
+++ class.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import random
+
+class Card:
+    def __init__(self, val, suit):
+        self.val = val
+        self.suit = suit
+        if(suit == "H" or suit == "D"):
+            self.color = "RED"
+        else:
+            self.color = "BLACK"
+    def __str__(self):
+        suitSym = None
+        if(self.suit == "H"):
+            suitSym = "\u2661" # ♥︎
+        if(self.suit == "S"):
+            suitSym = "\u2660" # ♠
+        if(self.suit == "D"):
+            suitSym = "\u2662"
+        if(self.suit == "C"):
+            suitSym = "\u2663"
+        return "%s%s" % (self.val, suitSym)
+
+class Deck:
+    def __init__(self):
+        vals = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+        suits = ['H','S','D','C']
+        self.stack = []
+        for val in vals:
+            for suit in suits:
+                self.stack.append(Card(val, suit))
+        random.shuffle(self.stack)
+    def __len__(self):
+        return len(self.stack)
+    def deal(self):
+        if(not len(self)):
+            return None
+        return self.stack.pop()
+
+
+deck = Deck()
+print(deck.deal())
+print([].pop())
